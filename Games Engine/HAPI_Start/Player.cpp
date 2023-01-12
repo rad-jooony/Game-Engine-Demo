a3b5db5e0404d118c8fde75c06819a05d63ff653
rad-jooony <shrimp.lad@gmail.com>
2023-01-12 21:15:16 +0000

    final

--- Games Engine/HAPI_Start/Player.cpp
+++ Games Engine/HAPI_Start/Player.cpp
@@ -2,9 +2,6 @@
 #include "Visualisation.h"
 void Player::Update(World& world)
 {
-	int leftThumbX = m_contData.analogueButtons[HK_ANALOGUE_LEFT_THUMB_X];
-	int leftThumbY = m_contData.analogueButtons[HK_ANALOGUE_LEFT_THUMB_Y];
-	bool contAttached = m_contData.isAttached;
 	
 	this->Movement(world);
 
@@ -19,25 +16,57 @@ void Player::Movement(World& world)
 {
 	p_velocity.x = 0;
 	p_velocity.y = 0;
-	//if (m_contData.isAttached);
+	
 
-	if (m_keyData.scanCode['A'])
+	int leftThumbX = m_contData.analogueButtons[HK_ANALOGUE_LEFT_THUMB_X];
+	int leftThumbY = m_contData.analogueButtons[HK_ANALOGUE_LEFT_THUMB_Y];
+	if (!m_contData.isAttached)
 	{
-		p_velocity.x -= p_speed;
+		if (m_keyData.scanCode['A'])
+		{
+			p_velocity.x -= p_speed;
+		}
+		if (m_keyData.scanCode['D'])
+		{
+			p_velocity.x += p_speed;
+		}
+		if (m_keyData.scanCode['W'])
+		{
+			p_velocity.y -= p_speed;
+		}
+		if (m_keyData.scanCode['S'])
+		{
+			p_velocity.y += p_speed;
+		}
 	}
-	if (m_keyData.scanCode['D'])
+	else
 	{
-		p_velocity.x += p_speed;
-	}
-	if (m_keyData.scanCode['W'])
-	{
-		p_velocity.y -= p_speed;
-	}
-	if (m_keyData.scanCode['S'])
-	{
-		p_velocity.y += p_speed;
-	}
+		if (leftThumbX > 1000 || leftThumbX < -1000) //ouside deadzone
+		{
 
+			if (leftThumbX > 500)
+			{
+				p_velocity.x -= p_speed;
+			}
+			if (leftThumbX < 500)
+			{
+				p_velocity.x += p_speed;
+			}
+		}
+		if (leftThumbY > 1000 || leftThumbY < -1000) //ouside deadzone
+		{
+
+			if (leftThumbY > 500)
+			{
+				p_velocity.y -= p_speed;
+			}
+			if (leftThumbY < 500)
+			{
+				p_velocity.y += p_speed;
+			}
+		}
+
+	}
 	// so diagonal movement is not faster than up/down, left/right
 	if (p_velocity.x != 0 && p_velocity.y != 0)
 	{
